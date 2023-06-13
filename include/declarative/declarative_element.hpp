763ee79c8db6e8d826a743005f279ed388beefa7
hungr <hungryfactory@icloud.com>
2023-06-13 23:30:35 +0900

    feat: add DeclarativeElement which is the base of ui object

---
+++ include/declarative/declarative_element.hpp
@@ -0,0 +1,11 @@
+#ifndef DECLARATIVE_UI_DECLARATIVE_DECLARATIVE_ELEMENT_HPP
+#define DECLARATIVE_UI_DECLARATIVE_DECLARATIVE_ELEMENT_HPP
+
+namespace declarative {
+    struct DeclarativeElement {
+        virtual ~DeclarativeElement() = default;
+
+    };
+}
+
+#endif
