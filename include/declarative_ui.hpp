31111e56593659d76158f45c57fe15767f420ac3
hungr <hungryfactory@icloud.com>
2023-06-16 10:42:56 +0900

    feat: implement root include file

--- include/declarative_ui.hpp
+++ include/declarative_ui.hpp
@@ -1,6 +1,8 @@
 #ifndef DECLARATIVE_UI_DECLARATIVE_UI_HPP
 #define DECLARATIVE_UI_DECLARATIVE_UI_HPP
 
-
+#include <declarative/declarative_element.hpp>
+#include <declarative/component.hpp>
+#include <declarative/container.hpp>
 
 #endif
