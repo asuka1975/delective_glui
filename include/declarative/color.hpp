039d49c71887ce5c139007c51d3f1bc7c976ecc1
hungr <hungryfactory@icloud.com>
2023-07-16 22:05:29 +0900

    feat: color style

---
+++ include/declarative/color.hpp
@@ -0,0 +1,13 @@
+#ifndef DECLARATIVE_UI_DECLARATIVE_COLOR_HPP
+#define DECLARATIVE_UI_DECLARATIVE_COLOR_HPP
+
+namespace declarative {
+    struct Color {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int a = 255;
+    };
+}
+
+#endif
--- include/declarative/style.hpp
+++ include/declarative/style.hpp
@@ -1,11 +1,12 @@
 #ifndef DECLARATIVE_UI_DECLARATIVE_STYLE_HPP
 #define DECLARATIVE_UI_DECLARATIVE_STYLE_HPP
 
+#include "declarative/color.hpp"
 namespace declarative {
     struct Style {
         int width = 100;
         int height = 100;
-        
+        Color background_color = {};
     };
 }
 
